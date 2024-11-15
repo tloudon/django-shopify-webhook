c9750307737249dc2540c7bc20e5fefdf6d1de81
Tim Loudon <tim@loudonco.com>
2024-11-15 12:29:02 -0800

    dirty fix for api key name collision

--- shopify_webhook/decorators.py
+++ shopify_webhook/decorators.py
@@ -40,7 +40,7 @@ def webhook(f):
             return HttpResponseBadRequest()
 
         # Verify the HMAC.
-        if not hmac_is_valid(request.body, settings.SHOPIFY_APP_API_SECRET, hmac):
+        if not hmac_is_valid(request.body, settings.SHOPIFY_CUSTOM_APP_API_SECRET, hmac):
             return HttpResponseUnauthorized()
 
         # Otherwise, set properties on the request object and return.
@@ -77,7 +77,7 @@ def carrier_request(f):
             return HttpResponseBadRequest()
 
         # Verify the HMAC.
-        if not hmac_is_valid(request.body, settings.SHOPIFY_APP_API_SECRET, hmac):
+        if not hmac_is_valid(request.body, settings.SHOPIFY_CUSTOM_APP_API_SECRET, hmac):
             return HttpResponseForbidden()
 
         # Otherwise, set properties on the request object and return.
@@ -97,7 +97,7 @@ def app_proxy(f):
     def wrapper(request, *args, **kwargs):
 
         # Verify the signature.
-        if not proxy_signature_is_valid(request, settings.SHOPIFY_APP_API_SECRET):
+        if not proxy_signature_is_valid(request, settings.SHOPIFY_CUSTOM_APP_API_SECRET):
             return HttpResponseBadRequest()
 
         return f(request, *args, **kwargs)
